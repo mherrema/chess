4ac8d60b05be19ebf10bf9483d2207c8ac6b86dd
mumfordr <mumfordr@mail.gvsu.edu>
2014-03-23 22:14:01 -0400

    Real commit - please use this one now
    got commit back

--- src/gvprojects/chess/model/Move.java
+++ src/gvprojects/chess/model/Move.java
@@ -4,6 +4,7 @@ package gvprojects.chess.model;
  * This class is the logic for moving a chess piece on a board
  */
 public class Move {
+<<<<<<< HEAD
 	/*
 	 * Initialize the variables
 	 */
@@ -13,6 +14,18 @@ public class Move {
 	 * The logic for moving a chess piece
 	 */
 	public Move(int fromRow, int fromColumn, int toRow, int toColumn) {
+=======
+	
+	/** Initialize the variables. */
+	@SuppressWarnings("unused")
+	protected int fromRow, fromColumn, toRow, toColumn;
+
+	/******************************************************************
+	 * The logic for moving a chess piece.
+	 ******************************************************************/
+	public Move(final int fromRow, final int fromColumn, 
+			final int toRow, final int toColumn) {
+>>>>>>> parent of 9282353... Checkstyle Fixes
 		this.fromRow = fromRow;
 		this.fromColumn = fromColumn;
 		this.toRow = toRow;
