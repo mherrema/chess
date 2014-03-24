d68b2b68b7443c11dead8e9bb55ca53ca7353d6a
mherrema <mherrema@yahoo.com>
2014-03-24 00:13:40 -0400

    Forced again
    this is annoying

--- src/gvprojects/chess/presenter/Presenter.java
+++ src/gvprojects/chess/presenter/Presenter.java
@@ -10,67 +10,87 @@ import gvprojects.chess.model.Model;
 import gvprojects.chess.model.Move;
 import gvprojects.chess.view.GUI;
 
+/**
+ * presenter class.
+ * 
+ * @author raleighmumford
+ *
+ */
 public class Presenter {
 
-	/** model */
+	/** model. */
 	private Model m;
 
-	/** view */
+	/** view. */
 	private GUI g;
-
+	/** from. */
 	private boolean from;
+	/** temp. */
 	private String temp;
-
+	/** fromRow. */
 	private int fromRow;
+	/** fromCol. */
 	private int fromCol;
+	/** toRow. */
 	private int toRow;
+	/** toCol. */
 	private int toCol;
 
 	/*********************************************************************
-	 * ActionListener and ActionPerformed for array
-	 * 
-	 * @return none
+	 * ActionListener and ActionPerformed for array.
 	 *********************************************************************/
-	class boardListener implements ActionListener {
-
+	class BOARDLISTENER implements ActionListener {
+		/** buttonRow. */
 		private int buttonRow;
+		/** buttonCOL. */
 		private int buttonCol;
-
-		boardListener(int buttonRow, int buttonCol) {
-			this.buttonRow = buttonRow;
-			this.buttonCol = buttonCol;
+		
+		/*********************************************************************
+		 * BoardListener for the code.
+		 * 
+		 * @param pbuttonRow int
+		 * @param pbuttonCol int
+		 *********************************************************************/
+		BOARDLISTENER(final int pbuttonRow, final int pbuttonCol) {
+			this.buttonRow = pbuttonRow;
+			this.buttonCol = pbuttonCol;
 		}
 
-		public void actionPerformed(ActionEvent e) {
+		/*********************************************************************
+		 * ActionPerformed for array.
+		 * 
+		 * @param e ActionEvent
+		 *********************************************************************/
+		public void actionPerformed(final ActionEvent e) {
 
-			if(isFrom() && hasPiece(buttonRow, buttonCol)){
+			if (isFrom() && hasPiece(buttonRow, buttonCol)) {
 				setFromRow(buttonRow);
 				setFromCol(buttonCol);
 				g.getBoard()[buttonRow][buttonCol].setBackground(Color.blue);
 				g.getBoard()[buttonRow][buttonCol].setOpaque(true);
 				from = false;
-			}
-			else{
-
+			} else {
 				setToRow(buttonRow);
 				setToCol(buttonCol);
 				Move tempMove = new Move(fromRow, fromCol, toRow, toCol);
 				m.move(tempMove);
 				
-				g.getBoard()[tempMove.fromRow][tempMove.fromColumn].setBackground(Color.LIGHT_GRAY);
-				g.getBoard()[tempMove.fromRow][tempMove.fromColumn].setOpaque(false);
+				g.getBoard()[tempMove.fromRow][tempMove.fromColumn]
+						.setBackground(Color.LIGHT_GRAY);
+				g.getBoard()[tempMove.fromRow][tempMove.fromColumn]
+						.setOpaque(false);
 				g.printBoard(m.getBoard());
-				if(m.isPutSelfInCheck()){
+				if (m.isPutSelfInCheck()) {
 					g.putSelfInCheck();
 					m.setPutSelfInCheck(false);
 				}
-				if(m.isOtherPlayerCheck()) {
+				if (m.isOtherPlayerCheck()) {
 					g.inCheck();
 					m.setOtherPlayerCheck(false);
 					m.switchTurns();
 				}
-				if(m.inCheck(m.currentPlayer())){
-				if(m.isComplete()){
+				if (m.inCheck(m.currentPlayer())) {
+				if (m.isComplete()) {
 					g.checkmate(m.getWinner());
 				}
 				//g.printBoard(m.getBoard());
@@ -81,7 +101,15 @@ public class Presenter {
 			}
 		}
 
+<<<<<<< HEAD
 
+=======
+	}
+	
+	/*********************************************************************
+	 * Constructor.
+	 *********************************************************************/
+>>>>>>> FETCH_HEAD
 	public Presenter() {
 		m = new Model();
 		g = new GUI();
@@ -94,21 +122,22 @@ public class Presenter {
 		// adds action listeners to all the buttons in the array
 		for (int i = 0; i < g.getBoard().length; i++) {
 			for (int j = 0; j < g.getBoard()[0].length; j++) {
-				g.addButtonListener(i, j, new boardListener(i,
+				g.addButtonListener(i, j, new BOARDLISTENER(i,
 						j));
 			}
 		}
 
 		// adds action listener to the quit button
 		g.getQuit().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				int option = JOptionPane.showConfirmDialog(g.getFrame(),
 						"Are you sure you want to quit?", "Exit Game",
 						JOptionPane.YES_NO_OPTION);
 				if (option == 0) {
 					System.exit(0);
 				} else {
-					g.setTextString(": Game resumed.\n---------------\n" + g.getTextString());
+					g.setTextString(": Game resumed.\n---------------\n" 
+								+ g.getTextString());
 					temp = m.currentPlayer().toString().toLowerCase();
 					temp = temp.substring(0, 1).toUpperCase()
 							+ temp.substring(1, temp.length()) + "'s turn.\n";
@@ -120,12 +149,13 @@ public class Presenter {
 
 		// adds action listener to the quit button
 		g.getNewGame().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				int option = 0;
-				if (!m.isComplete())
+				if (!m.isComplete()) {
 					option = JOptionPane.showConfirmDialog(g.getFrame(),
 							"All current progress will be lost, continue?",
 							"New Game", JOptionPane.YES_NO_OPTION);
+				}
 				if (option == 0) {
 					m = new Model();
 					g.printBoard(m.getBoard());
@@ -137,7 +167,8 @@ public class Presenter {
 					g.setText();
 					g.resetTime();
 				} else {
-					g.setTextString(": Game resumed.\n*************************\n" + g.getTextString());
+				  g.setTextString(": Game resumed.\n*************************\n"
+								+ g.getTextString());
 					temp = m.currentPlayer().toString().toLowerCase();
 					temp = temp.substring(0, 1).toUpperCase()
 							+ temp.substring(1, temp.length()) + "'s turn.\n";
@@ -149,7 +180,7 @@ public class Presenter {
 
 		// adds action listener to the quit button
 		g.getVersion().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				g.setTextString("*************************\n"
 						+ "       Awesome Chess\n           Version 2.0\n"
 						+ "*************************\n" + g.getTextString());
@@ -166,17 +197,19 @@ public class Presenter {
 		 * with a web site detailing the rules of chess
 		 */
 		g.getRules().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				try {
 					java.awt.Desktop
 					.getDesktop()
 					.browse(java.net.URI
-							.create("http://www.chess.com/learn-how-to-play-chess"));
-					g.setTextString(": Rules opened in \n: default browser.\n*************************\n"
+					   .create("http://www.chess.com/learn-how-to-play-chess"));
+					g.setTextString(": Rules opened in \n: "
+							+ "default browser.\n*************************\n"
 							+ g.getTextString());
 
 				} catch (Exception ex) {
-					g.setTextString(": Page not found.\n*************************\n" + g.getTextString());
+				g.setTextString(": Page not found.\n*************************\n"
+				+ g.getTextString());
 				}
 				temp = m.currentPlayer().toString().toLowerCase();
 				temp = temp.substring(0, 1).toUpperCase()
@@ -187,60 +220,139 @@ public class Presenter {
 		});
 	}
 
+<<<<<<< HEAD
 
+=======
+	/******************************************************************
+	 * Updates the gameboard.
+	 ******************************************************************/
+//	public final void update() {
+//		g.printBoard(m.getBoard());
+//		// if they put themselves in check
+//		if (m.isPutSelfInCheck()) {
+//			g.putSelfInCheck();
+//			m.setPutSelfInCheck(false);
+//		}
+//		// if they put the other player in check
+//		if (m.isOtherPlayerCheck()) {
+//			g.inCheck();
+//			m.setOtherPlayerCheck(false);
+//		}
+//		g.prompt(m.currentPlayer());
+//
+//	}
+>>>>>>> FETCH_HEAD
 
-	public static void main(String[] args) {
+	/**
+	 * main.
+	 * 
+	 * @param args parameter
+	 */
+	public static void main(final String[] args) {
 		Presenter p = new Presenter();
 		p.g.printBoard(p.m.getBoard());
 		p.g.updateCurrentPlayer(p.m.currentPlayer());
 	}
-
-	public boolean hasPiece(int r, int c){
-		if(m.getBoard()[r][c]!=null){
-			return true;
-		}
-		else{
-			return false;
-		}
+	
+	/**
+	 * hasPiece.
+	 * 
+	 * @param r param
+	 * @param c param
+	 * 
+	 * @return boolean
+	 */
+	public final boolean hasPiece(final int r, final int c) {
+		return m.getBoard()[r][c] != null;
 	}
-
-	public boolean isFrom() {
+	
+	/** 
+	 * isFrom.
+	 * 
+	 * @return boolean
+	 */
+	public final boolean isFrom() {
 		return from;
 	}
 
-	public void setFrom(boolean from) {
-		this.from = from;
+	/** 
+	 * setFrom.
+	 * 
+	 * @param pfrom boolean
+	 */
+	public final void setFrom(final boolean pfrom) {
+		this.from = pfrom;
 	}
 
-	public int getFromRow(){
+	/**
+	 * getFromRow.
+	 * 
+	 * @return fromRow int
+	 */
+	public final int getFromRow() {
 		return fromRow;
 	}
 
-	public void setFromRow(int r){
+	/**
+	 * getFromRow.
+	 * 
+	 * @param r int
+	 */
+	public final void setFromRow(final int r) {
 		fromRow = r;
 	}
 
-	public int getFromCol(){
+	/**
+	 * getFromRow.
+	 * 
+	 * @return fromRow int
+	 */
+	public final int getFromCol() { 
 		return fromRow;
 	}
 
-	public void setFromCol(int c){
+	/**
+	 * getFromRow.
+	 * 
+	 * @param c int
+	 */
+	public final void setFromCol(final int c) {
 		fromCol = c;
 	}
 
-	public int getToRow(){
+	/**
+	 * getFromRow.
+	 * 
+	 * @return fromRow int
+	 */
+	public final int getToRow() {
 		return fromRow;
 	}
 
-	public void setToRow(int r){
+	/**
+	 * getFromRow.
+	 * 
+	 * @param r int
+	 */
+	public final void setToRow(final int r) {
 		toRow = r;
 	}
 
-	public int getToCol(){
+	/**
+	 * getFromRow.
+	 * 
+	 * @return fromRow int
+	 */
+	public final int getToCol() {
 		return fromRow;
 	}
-
-	public void setToCol(int c){
+	
+	/**
+	 * getFromRow.
+	 * 
+	 * @param c int
+	 */
+	public final void setToCol(final int c) {
 		toCol = c;
 	}
 }
